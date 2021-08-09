c8857827908c81df0355b94aaf7f415c43635074
waki-h <88266369+waki-h@users.noreply.github.com>
2021-08-09 23:02:24 +0900

    Update portfolio.js

--- js/portfolio.js
+++ js/portfolio.js
@@ -10,12 +10,6 @@ $(function() {
     }
   });
 
-  $('.blackBg').click(function() {                                         //黒い背景をクリックしたら
-      $("#drawerToggle").toggleClass("open");　　　　　　　　　　　　　　　　//ハンバーガーメニューが閉じる
-      $(".spNav,.blackBg").removeClass("sideOpen");
-      $('body,html').css({"overflow":"visible","height":"auto"})　　　　　//画面スクロールできるようになる
-  });
-
 
   $(window).on("resize", function() {
     var w = $(window).width();
