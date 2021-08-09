abcb24139451c60bb4796b02cd982b9131aad463
waki-h <88266369+waki-h@users.noreply.github.com>
2021-08-09 23:13:50 +0900

    Update portfolio.js

--- js/portfolio.js
+++ js/portfolio.js
@@ -3,11 +3,13 @@ $(function() {
   $("#drawerToggle").click(function(){
     $(this).toggleClass("open");
     $(".spNav,.blackBg").toggleClass("sideOpen");
-    if($(this).hasClass("open") == false) {　　　　　　　　　                 //もしopenclassが付いてなかったら
-      $('body,html').css({"overflow":"visible","height":"auto"});　　　　　　//画面スクロールできる
-    }else {　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　//そうじゃない時は
-      $('body,html').css({"overflow":"hidden","height":"100%"});   　　　　//背景スクロールできない
-    }
+
+  });
+
+  $('.blackBg').click(function() {                                         //黒い背景をクリックしたら
+      $("#drawerToggle").toggleClass("open");　　　　　　　　　　　　　　　　//ハンバーガーメニューが閉じる
+      $(".spNav,.blackBg").removeClass("sideOpen");
+      $('body,html').css({"overflow":"visible","height":"auto"})　　　　　//画面スクロールできるようになる
   });
 
 
